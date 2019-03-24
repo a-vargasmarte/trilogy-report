8a54991e5a1a8efdb1a5a69b5a33d82582fdc5f0
a-vargasmarte <a.vargasmarte@gmail.com>
2019-03-24 12:27:55 -0400

    added virtual environment and established connection between react and flask

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get("/hello")
+      .then(res => {
+        console.log(res);
+        // this.setState({ greeting: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
