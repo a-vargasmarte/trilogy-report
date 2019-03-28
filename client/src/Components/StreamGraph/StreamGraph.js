dd25bc500bfb9f7a9296204778551c2990cbc80f
a-vargasmarte <a.vargasmarte@gmail.com>
2019-03-28 14:53:54 -0400

    parsed time data

---
+++ client/src/Components/StreamGraph/StreamGraph.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import {
+  stack,
+  area,
+  curveBasis,
+  stackOrderInsideOut,
+  stackOffsetSilhouete
+} from "d3-shape";
+import { range } from "d3-array";
+import { scaleLinear } from "d3-scale";
+
+class StreamGraph extends Component {
+    state = {  }
+    render() {
+        
+        const stackData = range(30).map(() => ({}));
+
+        for (let x = 0; x < 30; x++) {
+            this
+        }
+        
+
+        return (  );
+    }
+}
+ 
+export default StreamGraph;
